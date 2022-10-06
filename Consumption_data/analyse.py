29bb25b542514d9f2116e2f2fe0f51ed08a04e28
omaxim <maxim.oweyssi@gmail.com>
2022-10-06 19:30:16 +0100

    final analysis

--- Consumption_data/analyse.py
+++ Consumption_data/analyse.py
@@ -15,30 +15,34 @@ uncontrolable = pd.DataFrame(uncontrolable_data)/1000 # in GW
 time_shift_dict = {'MK':0, 'FR':-1, 'SE':0, 'UK':-1, 'FI':0, 'RS':0, 'AT':0, 'IT':0, 'NO':0, 'RO':0, 'DE':0, 'NL':-1, 'ME':0, 'CZ':0, 'LV':0, 'CH':0, 'PT':-1, 'BE':-1, 'IE':-1, 'SI':0, 'SK':0, 'ES':-1, 'PL':0, 'EE':0, 'LT':0, 'HR':0, 'GR':-1, 'HU':0, 'DK':0, 'BG':0, 'BA':0}
 
 hours_in_day = 24
-fig, ax = plt.subplots(figsize=(7, 7))
+fig, ax = plt.subplots(figsize=(9, 7))
 hours_x = np.arange(hours_in_day)
 #ax.plot(hours_x, controlable.sum(axis=1), label='controlable sources before')
 #ax.plot(hours_x, uncontrolable.sum(axis=1), label='uncontrolable sources')
-ax.plot(hours_x, uncontrolable.sum(axis=1) + controlable.sum(axis=1), label='total before')
 
+total_before = uncontrolable.sum(axis=1) + controlable.sum(axis=1)
 for country, time_shift in time_shift_dict.items():
     # this if statement is kinda reduntant
     # if country in controlable.columns:
     controlable[country] = controlable[country].reindex(index=np.roll(controlable[country].index, time_shift))
     controlable[country] = np.roll(controlable[country], time_shift)
-
+total_after = uncontrolable.sum(axis=1) + controlable.sum(axis=1)
 
 #ax.plot(hours_x, controlable.sum(axis=1), label='controlable sources after')
-ax.plot(hours_x, uncontrolable.sum(axis=1) + controlable.sum(axis=1), label='total after')
+ax.plot(hours_x, total_before, label='total before')
+ax.plot(hours_x, total_after, label='total after')
 ax.set_xbound(0, hours_in_day - 1)
 ax.set_xticks(np.arange(0, hours_in_day, step=1))
 
 
 ax.set(xlabel='Time [Hours]', ylabel='Net Generation (MW)',
-       title='Winter energy consumption during the day in Europe')
+       title='Winter energy production during the day in Europe')
 ax.grid()
-ax.legend()
-plt.show()
+ax.legend(loc=2)
+ax2 = ax.twinx()
+ax2.plot(hours_x,100*(total_after/total_before-1),label = 'percentage change',color='grey',alpha=0.5,linewidth=3)
+ax2.legend(loc=1)
 
+plt.tight_layout()
 plt.show()
 
