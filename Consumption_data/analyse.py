21050bfe73110f9f5db62dc4180836778ec50d3d
Mateusz Meller <mateusz.meller2@gmail.com>
2022-11-10 14:22:58 +0000

    Added constraints to the optimization.

--- Consumption_data/analyse.py
+++ Consumption_data/analyse.py
@@ -1,3 +1,4 @@
+from os import umask
 import numpy as np
 import glob
 import pandas as pd
@@ -14,7 +15,32 @@ uncontrolable_data = {country_names[i]:np.load(countries[i])[2,:] for i in range
 controlable = pd.DataFrame(controlable_data)/1000 # in GW
 uncontrolable = pd.DataFrame(uncontrolable_data)/1000 # in GW
 
-time_shift_dict = {'MK':0, 'FR':-1, 'SE':0, 'UK':-1, 'FI':0, 'RS':0, 'AT':0, 'IT':0, 'NO':0, 'RO':0, 'DE':0, 'NL':-1, 'ME':0, 'CZ':0, 'LV':0, 'CH':0, 'PT':-1, 'BE':-1, 'IE':-1, 'SI':0, 'SK':0, 'ES':-1, 'PL':0, 'EE':0, 'LT':0, 'HR':0, 'GR':-1, 'HU':0, 'DK':0, 'BG':0, 'BA':0}
+# time_shift_dict = {
+#     'MK':0, 'FR':-1, 'SE':0, 'UK':-1, 'FI':0,'RS':0,
+#     'AT':0, 'IT':0, 'NO':0, 'RO':0, 'DE':0, 'NL':-1, 
+#     'ME':0, 'CZ':0, 'LV':0, 'CH':0, 'PT':-1, 'BE':-1, 
+#     'IE':-1, 'SI':0, 'SK':0, 'ES':-1, 'PL':0, 'EE':0, 
+#     'LT':0, 'HR':0, 'GR':-1, 'HU':0, 'DK':0, 'BG':0, 'BA':0
+# }
+
+# portugal, ireland,
+# spain, uk
+# france, belgium, netherlands, switz  
+# italy, germany, norway, denmark
+# croatia, slovenia,  austria, czechia, sweden 
+# greece, n macedonia, montenegro, bosnia, serbia, hungary, slovakia, poland
+# bulgaria, romania,  lithauania, latvia, estonia, finland 
+
+time_shift_dict = {
+    'PT': -1, 'IE': -1, 'ES': -1, 'UK': -1,
+    'FR': -1, 'BE': -1, 'NL': -1, 'CH': 0,
+    'IT': 0, 'DE': 0, 'NO': 0, 'DK': 0,
+    'HR': 0, 'SI': 0, 'AT': 0, 'CZ': 0, 
+    'SE': 0, 'GR': -1, 'MK': 0, 'ME': 0, 
+    'BA': 0, 'RS': 0, 'HU': 0, 'SK': 0, 
+    'PL': 0, 'BG': 0, 'RO': 0, 'LT': 0, 
+    'LV': 0, 'EE': 0, 'FI': 0, 
+}
 
 hours_in_day = 24
 fig, ax = plt.subplots(figsize=(9, 7))
@@ -33,24 +59,20 @@ def objective(x, time_shift_dict, controlable):
 def max_objective(x, time_shift_dict, controlable):
     return max(objective(x, time_shift_dict, controlable))
 
-# Set constraints functions, for example
-# were x[0] corresponds to MK, x[1] to FR etc.
-def con1(x):
-    return x[0] < x[1]
-
-def con2(x):
-    return x[1] < x[2]
-
-# TODO: do the constraints for all countries
-cons = ({'type':'ineq', 'fun': con1},
-        {'type':'ineq', 'fun': con2},)
+# Set constraints functions, for example where x[0] corresponds to PT, x[1] to IE etc.
+# I have ordered countries in time_shift_dict that they go from west -> east
+# basically, each country on the east must be lower or equal than on the right.
+constraints = []
+for i in range(len(time_shift_dict)):
+    for j in range(i+1, len(time_shift_dict)):
+        constraints.append({'type': 'ineq', 'fun': lambda x: x[i] <= x[j]})
 
 total_before = uncontrolable.sum(axis=1) + controlable.sum(axis=1)
 
 # Only COBYLA works, but it doesn't accept bounds
 # Can play with optim parameters here
 optim_solution = minimize(max_objective, x, (time_shift_dict, controlable), 'COBYLA',
-                          constraints=cons, tol=1e-5, options={'maxiter': 1000, 'disp': True})
+                          constraints=constraints, tol=1e-5, options={'maxiter': 1000, 'disp': True})
 x = optim_solution.x
 x = [int(round(x_i)) for x_i in x]
 print(x)
